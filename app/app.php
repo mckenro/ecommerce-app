97cb8f102e0908ebf5a899624cc2f3d4d97949c6
Rob McKenzie <mckenro@gmail.com>
2017-03-19 17:10:21 -0700

    add functioning route for index page

--- app/app.php
+++ app/app.php
@@ -1,16 +1,17 @@
 <?php
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__.'/../vendor/autoload.php';
     // CHANGE TO BE A PRODUCT OBJECT WITH PHP require_once __DIR__.'/../src/Contact.php';
 
-    session_start();
-    if (empty($_SESSION['list_of_contacts'])){
-        $_SESSION['list_of_contacts'] = array();
-    }
+    // session_start();
+    // if (empty($_SESSION['list_of_contacts'])){
+    //     $_SESSION['list_of_contacts'] = array();
+    // }
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig', array('contacts' => Contact::getAll()));
+        return $app['twig']->render('home.html.twig');
 });
 
 
